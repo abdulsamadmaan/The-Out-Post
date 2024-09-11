55b51d788937e48f40018a24f353825905920067
abdulsamadmaan <abdulsamadmaan3@gmail.com>
2024-09-12 02:32:13 +0500

    Verify-the-nav-bar

--- cypress/e2e/theone.cy.js
+++ cypress/e2e/theone.cy.js
@@ -13,5 +13,20 @@ describe('visit page', () => {
         cy.get('.gap-3 > a > img').should('be.visible')
     })
 
+    it('verify the nav bar', () => {
+        cy.viewport(1920, 1080)
 
+
+        cy.get('.h-12 > .relative > .items-center > div').should('be.visible')
+        cy.get('.h-12 > :nth-child(3) > a').should('be.visible')
+        cy.get('.h-12 > :nth-child(4) > a').should('be.visible')
+        cy.get('.h-12 > :nth-child(5) > a').should('be.visible')
+            // cy.get('.navbar-nav > :nth-child(5) > .nav-link').should('be.visible')
+        cy.wait(8000)
+
+    })
+
+    it('verify the text of nav bar CTA', () => {
+        cy.get
+    })
 })
